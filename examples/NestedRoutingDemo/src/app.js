a7488c36fbf47f84bf3080d3c139a6424141169b
brucou <bruno.couriol@gmail.com>
2017-08-23 04:41:26 +0200

    upated TODO

--- examples/NestedRoutingDemo/src/app.js
+++ examples/NestedRoutingDemo/src/app.js
@@ -6,7 +6,8 @@ import { HomePage } from "./HomePage"
 import { ROUTE_SOURCE } from "../../../src/components/Router/properties"
 import { AspirationalPageHeader, Card, CardDetail } from "./AspirationalPage"
 import {
-  BLACBIRD_CARD_INFO, BLACK_BIRD_DETAIL_ROUTE, TECHX_CARD_INFO, TYPOGRAPHICS_CARD_INFO, TECHX_CARD_DETAIL_ROUTE, TYPOGRAPHICS_CARD_DETAIL_ROUTE
+  BLACBIRD_CARD_INFO, BLACK_BIRD_DETAIL_ROUTE, TECHX_CARD_DETAIL_ROUTE, TECHX_CARD_INFO,
+  TYPOGRAPHICS_CARD_DETAIL_ROUTE, TYPOGRAPHICS_CARD_INFO
 } from "./properties"
 
 const $ = Rx.Observable;
@@ -30,28 +31,37 @@ export const App = InjectSourcesAndSettings({
   sourceFactory: injectRouteSource,
   settings: {
     sinkNames: [DOM_SINK, 'router'],
-    routeSource : ROUTE_SOURCE,
+    routeSource: ROUTE_SOURCE,
     trace: 'App'
   }
 }, [
   OnRoute({ route: '', trace: 'OnRoute (/)' }, [
     HomePage
   ]),
-    OnRoute({ route: 'aspirational', trace: 'OnRoute  (aspirational)' }, [
-      m({ makeOwnSinks: AspirationalPageHeader }, { breadcrumbs: ['aspirational'] }, [
-        Card(BLACBIRD_CARD_INFO),
-        OnRoute({route: BLACK_BIRD_DETAIL_ROUTE},[
-          CardDetail(BLACBIRD_CARD_INFO)
-        ]),
-        Card(TECHX_CARD_INFO),
-        OnRoute({route: TECHX_CARD_DETAIL_ROUTE},[
-          CardDetail(TECHX_CARD_INFO)
-        ]),
-        Card(TYPOGRAPHICS_CARD_INFO),
-        OnRoute({route: TYPOGRAPHICS_CARD_DETAIL_ROUTE},[
-          CardDetail(TYPOGRAPHICS_CARD_INFO)
-        ]),
-      ])
-    ]),
+  OnRoute({ route: 'aspirational', trace: 'OnRoute  (aspirational)' }, [
+    m({ makeOwnSinks: AspirationalPageHeader }, { breadcrumbs: ['aspirational'] }, [
+      Card(BLACBIRD_CARD_INFO),
+      OnRoute({ route: BLACK_BIRD_DETAIL_ROUTE, trace: `OnRoute (${BLACK_BIRD_DETAIL_ROUTE})` }, [
+        CardDetail(BLACBIRD_CARD_INFO)
+      ]),
+      Card(TECHX_CARD_INFO),
+      OnRoute({ route: TECHX_CARD_DETAIL_ROUTE, trace: `OnRoute (${TECHX_CARD_DETAIL_ROUTE})` }, [
+        CardDetail(TECHX_CARD_INFO)
+      ]),
+      Card(TYPOGRAPHICS_CARD_INFO),
+      OnRoute({
+        route: TYPOGRAPHICS_CARD_DETAIL_ROUTE, trace: `OnRoute (${TYPOGRAPHICS_CARD_DETAIL_ROUTE})`
+      }, [
+        CardDetail(TYPOGRAPHICS_CARD_INFO)
+      ]),
+    ])
+  ]),
 ]);
 
+// TODO : check logs:
+// - understand
+// - check no double display or recomputation (how?)
+// - check why router Router (OnRoute (/)) : params is null!!! is duplicated ?? sometimes 5 of them
+// TODO : home route / matching
+// - what if I want to put there a coponent which is always displayed? How to do it with this
+// formulat? Should I not switch to have '/' always matching instead of now not matching??
